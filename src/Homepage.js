0694a085c0f86c613f36fb27eaf60cee67576895
yaohuangguan <yaob@miamioh.edu>
2019-10-20 11:31:28 +0800

    added init

---
+++ src/Homepage.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <div className='Homepage'>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Kobe Byrant</h1>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Lebron James</h1>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Derrick Rose</h1>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>Vince Carter</h1>
+            <span className='subtitle'>Shop Now</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
