a8ba94b12117db64669dd5284a3b3311d7987939
yaohuangguan <yaob@miamioh.edu>
2019-11-14 11:13:16 +0800

    Updates

---
+++ precache-manifest.b56ce0a7b2ec8fdf14d58981a59b6d77.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01eeaf3efc9ca90ac7bcb902b63e735a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c1621884cdc9fd8e9b8",
+    "url": "/static/css/3.3abbc071.chunk.css"
+  },
+  {
+    "revision": "360b6582c14ff14d0072",
+    "url": "/static/css/4.e8a9e73e.chunk.css"
+  },
+  {
+    "revision": "ed13631c0f5addde2796",
+    "url": "/static/css/5.e8a9e73e.chunk.css"
+  },
+  {
+    "revision": "327ef880b7b2af7024c5",
+    "url": "/static/css/6.fd29baf3.chunk.css"
+  },
+  {
+    "revision": "539b147bbc49d9e83354",
+    "url": "/static/css/7.fc9c57b3.chunk.css"
+  },
+  {
+    "revision": "dec04a4b9b03a0d2ac2e",
+    "url": "/static/css/main.a8199add.chunk.css"
+  },
+  {
+    "revision": "5f1c253247bd616dac05",
+    "url": "/static/js/2.cce766c7.chunk.js"
+  },
+  {
+    "revision": "7c1621884cdc9fd8e9b8",
+    "url": "/static/js/3.5a398503.chunk.js"
+  },
+  {
+    "revision": "360b6582c14ff14d0072",
+    "url": "/static/js/4.1fe5ce87.chunk.js"
+  },
+  {
+    "revision": "ed13631c0f5addde2796",
+    "url": "/static/js/5.fce5e882.chunk.js"
+  },
+  {
+    "revision": "327ef880b7b2af7024c5",
+    "url": "/static/js/6.856c0dbc.chunk.js"
+  },
+  {
+    "revision": "539b147bbc49d9e83354",
+    "url": "/static/js/7.4eee2916.chunk.js"
+  },
+  {
+    "revision": "dec04a4b9b03a0d2ac2e",
+    "url": "/static/js/main.b481c577.chunk.js"
+  },
+  {
+    "revision": "c9df2b1f09823368ee8f",
+    "url": "/static/js/runtime-main.f7127961.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pomelo/precache-manifest.551b166491b81809657a634eeaf075d9.js"
+  "/precache-manifest.b56ce0a7b2ec8fdf14d58981a59b6d77.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/pomelo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
